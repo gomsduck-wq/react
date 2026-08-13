561c427a14ecf133859d72f52ecbe2611d2c4ece
gomathi <gomsduck@gmail.com>
2026-08-13 12:56:24 +0530

    PROPS concept done

---
+++ my-react-app/src/Details.jsx
@@ -0,0 +1,18 @@
+import A from './Table.jsx'
+function Details(){
+    return(
+     <A   
+    Name="Gomathi R."
+    DateOfBirth ="18 December 2005"
+    Gender= "Female"
+    FathersName="Ramar"
+    MothersName= "Selvi"
+    Qualification= "BCA"
+    College= "Ayya Nadar Janaki Ammal College"
+    Location="Tamil Nadu, India"
+    Email= "gomsduck@gmail.com"
+    Phone= "+91 6381484980"
+    />
+    )
+}
+export default Details;
